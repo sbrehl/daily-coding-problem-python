fe8901c97bfdc612b107c8654d46ea4d1a652033
sbrehl <sascha.brehl@mail.de>
2022-05-25 16:22:50 +0200

    updated comments

--- daily_coding_problem13.py
+++ daily_coding_problem13.py
@@ -22,13 +22,27 @@ def func_longest_substring(string, dist_characters):
     for letter in string:
         if letter not in current_letters:
             current_letters.append(letter)
+            
+            # current_letters is my way of checking how many distinct characters
+            # are already in use in the current substring
         
         if len(current_letters) >= dist_characters+1:
             
             current_substring = ''
             current_letters = []
+            
+            # if the length of current_letters exceeds the number of distinct 
+            # characters that should occur, current_substring is deleted, so
+            # so that it can be build up again without skipping the current letter
                 
             length_longest_substring = func_longest_substring(string[1:], dist_characters)
+            
+            # the function is called recursively, with the first letter missing,
+            # so that no possible combination of letters is missed. If the loop
+            # were to simply go on, all the letters that were already looped over
+            # would be disregarded, and the algorithm would have "missed" a
+            # possible start of the longest substring
+            
         current_substring += letter
         
         if len(current_substring) > len(longest_substring):
@@ -39,8 +53,3 @@ def func_longest_substring(string, dist_characters):
                 
             
     return length_longest_substring
-
-s = "abababcdefgabababababab"
-k = 2
-
-print(func_longest_substring(s,k))
